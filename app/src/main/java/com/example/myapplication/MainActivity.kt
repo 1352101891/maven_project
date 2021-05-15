88ec2fb5a7b0babbf8b7766e602fd630e536b1f4
lvqiu <1352101891@qq.com>
2021-05-15 17:47:30 +0800

    app项目引用permissionUtil module

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.rong.pushperm.ResultCallback
+import io.rong.pushperm.RongPushPremissionsCheckHelper
+import io.rong.pushperm.perm.PermissionType
+import java.lang.reflect.Constructor
+
+class MyResultCallback():ResultCallback{
+    override fun onAreadlyOpened(value: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBeforeShowDialog(value: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onGoToSetting(value: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFailed(value: String?, type: ResultCallback.FailedType?) {
+        TODO("Not yet implemented")
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RongPushPremissionsCheckHelper.checkPermissionsAndShowDialog(
+                this
+                ,MyResultCallback())
     }
+
 
     public suspend fun t(){
         tt()
